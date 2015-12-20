d7fa4cf36a48dbc10cbcf6c3183f9f6279dd2e4b
Pratyush Poddar <pratyush.poddar@compassitesinc.com>
2015-12-20 23:34:55 +0530

    udpate response

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join('./')));
 
 app.get('/', function(request, response) {
   console.log('server');
-  response.send('hi pratyush!');
+  response.send('hi pratyush this is utsav!');
 });
 
 var server = app.listen(process.env.PORT || 3001, function() {
