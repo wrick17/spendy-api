89afaf2eab4a909e32f7a4026195c408a2fce9df
UtsavDawn <utsavdawn@gmail.com>
2016-01-06 13:09:28 +0530

    add production config

--- config.js
+++ config.js
@@ -1,10 +1,6 @@
 module.exports = {
-
-    //'secret': 'paperboatapi',
-    //'database': 'mongodb://localhost/spendy',
     'database': 'mongodb://utsavdawn:utsav12@ds033915.mongolab.com:33915/spendy',
     'port': 3000,
-    //'tokenExpiryTime': 1440,    
     'SUCCESS_MSG': "Success",    
     'FAILURE_MSG': "Failure",
     'DUPLICATE_NOT_ALLOWED': 'Duplicate entry not allowed'
